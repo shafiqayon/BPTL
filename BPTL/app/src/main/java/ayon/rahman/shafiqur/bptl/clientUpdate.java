03aca0c5801b342641e312b291fe4641b55c0d50
shafiqurrahmanayon <shafiqur.rahman.ayon@gmail.com>
2016-04-18 23:58:39 +0600

    some ui tweaks

---
+++ BPTL/app/src/main/java/ayon/rahman/shafiqur/bptl/clientUpdate.java
@@ -0,0 +1,13 @@
+package ayon.rahman.shafiqur.bptl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class clientUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_update);
+    }
+}
